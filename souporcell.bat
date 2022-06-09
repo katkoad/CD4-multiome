c6e56b09200a0f5436fbc28d6f004c2a85259015
Katko <katy2h@ea22-00139.chmccorp.cchmc.org>
2022-06-09 11:29:53 -0400

    Adding the scripts I use with the project

--- souporcell.bat
+++
@@ -1,8 +0,0 @@
-#BSUB -W 10:00
-#BSUB -n 10
-
-module load souporcell
-
-cd /data/weirauchlab/collab/miraldi/kat6ti/Barski_Multiome/data/resting_combined_set2
-
-souporcell_pipeline.py -i gex_possorted_bam.bam -b /raw_feature_bc_matrix/barcodes.tsv -f /data/weirauchlab/databank/genomes/hg38/hg38.fa -o souporcell_out -k 2
